fdbac2abac62e3456116bbba597185200afccd08
Jei <cerpajeison9@gmail.com>
2024-08-23 11:01:13 -0500

    Primer Ejecicio JS

--- FASE 2/JavaScript/Script/Ejercicio1.js
+++ FASE 2/JavaScript/Script/Ejercicio1.js
@@ -1,9 +1,11 @@
 var nombre = 'Jeison';
 var apellido = 'Cerpa';
 var apellido2 = 'Osorio';
+
 var nombre1 = document.getElementById("nombre");
 var apellido11 = document.getElementById("apellido");
 var apellido22 = document.getElementById("apellido2");
+
 nombre1.innerHTML = `El nombre es: ${nombre}`;
 apellido11.innerHTML = `El primer apellido es: ${apellido}`;
 apellido22.innerHTML = `El segundo apellido es: ${apellido2}`;
