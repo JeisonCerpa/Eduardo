e68c7b36aa1f8382c0d06d6d35da726b13945890
Jei <cerpajeison9@gmail.com>
2024-09-09 08:27:09 -0500

    Agregar inserción de empleado en la base de datos

--- FASE 2/Python/CONEX BASE DE DATOS/ejemplo.py
+++ FASE 2/Python/CONEX BASE DE DATOS/ejemplo.py
@@ -1,1 +1,11 @@
 import mysql.connector
+
+con =mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="ejemplo"
+)
+jei=con.cursor()
+jei.execute("insert into empleados (nombre, apellido, puesto, salario, fecha_contratacion) values (%s, %s, %s, %s, %s)", ("Jeison", "Cerpa", "Desarrollador", 25000000, "2021-09-01"))
+con.commit()
