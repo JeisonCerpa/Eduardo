93fa3b5e2856d6a00d024c7784f6eee41baa8c70
Jeison Cerpa <cerpajeison8@gmail.com>
2024-09-30 13:33:59 -0500

    Agregar funcionalidad de reporte de usuarios, clientes, productos, categorias y empleados

--- FASE 2/Python/CONEX BASE DE DATOS/ejercicio/index.py
+++ FASE 2/Python/CONEX BASE DE DATOS/ejercicio/index.py
@@ -25,6 +25,7 @@ while True:
             print("Bienvenido al sistema de usuarios.")
             print("1. Consultar")
             print("2. Insertar")
+            print("3. Reporte")
             print("0. Salir")
             seleccion = int(input("Selecciona una opcion: "))
             if seleccion == 1:
@@ -83,21 +84,32 @@ while True:
                     print("Usuario agregado correctamente.")
                     if input("¿Desea agregar otro usuario? (s/n): ") == "n":
                         break
+            elif seleccion == 3:
+                print("---Consulta de usuarios---")
+                tablas.execute("SELECT * FROM `usuarios`")
+                resultado = tablas.fetchall()
+                for i in resultado:
+                    print(f"Cédula: {i[0]}, Nombre: {i[1]}, Apellido: {i[2]}, Dirección: {i[3]}, Teléfono: {i[4]}, Correo: {i[5]}, Contraseña: {i[6]}")
+                input("Presione enter para continuar.")
             elif seleccion == 0:
                 break
     elif seleccion == 2:
         while True:
+            system("cls")
+            print("Bienvenido al sistema de clientes.")
             print("1. Consultar")
             print("2. Insertar")
+            print("3. Reporte")
             print("0. Salir")
             seleccion = int(input("Selecciona una opcion: "))
             if seleccion == 1:
                 while True:
-                    ingresar = input("Ingrese el ID del cliente: ")
+                    print("---Consulta de clientes---")
+                    ingresar = input("Ingrese la cedula: ")
                     tablas.execute("SELECT * FROM `clientes` WHERE cedula = %s;", (ingresar,))
                     resultado = tablas.fetchone()
                     if resultado:
-                        print(f"ID del cliente: {resultado[0]}, Nombre: {resultado[1]}, apellido: {resultado[2]}, direccion: {resultado[3]}, cedula: {resultado[4]} telefono: {resultado[5]} email: {resultado[6]} password: {resultado[7]}") 
+                        print(f"Cedula: {resultado[0]}, Nombre: {resultado[1]}, Apellido: {resultado[2]}, Direccion: {resultado[3]}, Telefono: {resultado[4]}, Email: {resultado[5]}") 
                         print("1. Para actualizar datos")
                         print("2. Para eliminar producto")
                         print("3. Para volver a consultar")
@@ -127,27 +139,36 @@ while True:
                     else:
                         print("No se encontró ningún producto con ese ID.")
             
-    elif seleccion == 2:
-        while True:
-            print("Ingrese los datos del usuario.")
-            cedula = input("Ingrese la cedula: ")
-            nombre = input("Ingrese el nombre: ")
-            apellido = input("Ingrese el apellido: ")
-            direccion = input("Ingrese la dirección: ")
-            email = input("Ingrese el email: ")
-            tablas.execute("INSERT INTO `clientes` (cedula, nombre, apellido, direccion, telefono, cedula, email) VALUES (%s, %s, %s, %s, %s, %s);", (cedula, nombre, apellido, direccion, telefono, cedula, email))
-            con.commit()
-            print(" producto agregado correctamente.")
-            if input("¿Desea agregar otro producto? (s/n): ") == "n":
+            elif seleccion == 2:
+                while True:
+                    print("Ingrese los datos del usuario.")
+                    cedula = input("Ingrese la cedula: ")
+                    nombre = input("Ingrese el nombre: ")
+                    apellido = input("Ingrese el apellido: ")
+                    telefono = input("Ingrese el telefono: ")
+                    direccion = input("Ingrese la dirección: ")
+                    email = input("Ingrese el email: ")
+                    tablas.execute("INSERT INTO `clientes` (cedula, nombres, apellidos, direccion, telefono, email) VALUES (%s, %s, %s, %s, %s, %s);", (cedula, nombre, apellido, direccion, telefono, email))
+                    con.commit()
+                    print(" producto agregado correctamente.")
+                    if input("¿Desea agregar otro producto? (s/n): ") == "n":
+                        break
+            elif seleccion == 3:
+                print("---Consulta de clientes---")
+                tablas.execute("SELECT * FROM `clientes`")
+                resultado = tablas.fetchall()
+                for i in resultado:
+                    print(f"Cedula: {i[0]}, Nombre: {i[1]}, apellido: {i[2]}, direccion: {i[3]}, cedula: {i[4]} telefono: {i[5]} email: {i[6]}")
+                input("Presione enter para continuar.")
+            elif seleccion == 0:
                 break
-    elif seleccion == 0:
-        break
     elif seleccion == 3:
         while True:
             system("cls")
             print("Bienvenido al sistema de productos.")
             print("1. Consultar")
             print("2. Insertar")
+            print("3. Reporte")
             print("0. Salir")
             seleccion = int(input("Selecciona una opcion: "))
             if seleccion == 1:
@@ -204,6 +225,13 @@ while True:
                     print("Producto agregado correctamente.")
                     if input("¿Desea agregar otro producto? (s/n): ") == "n":
                         break
+            elif seleccion == 3:
+                print("---Consulta de productos---")
+                tablas.execute("SELECT * FROM `productos`")
+                resultado = tablas.fetchall()
+                for i in resultado:
+                    print(f"ID del producto: {i[0]}, Nombre: {i[1]}, Descripcion: {i[2]}, Precio: {i[3]}, Fecha de Vencimiento: {i[4]}")
+                input("Presione enter para continuar.")
             elif seleccion == 0:
                 break
     elif seleccion == 4:
@@ -212,6 +240,7 @@ while True:
             print("Bienvenido al sistema de categorias.")
             print("1. Consultar")
             print("2. Insertar")
+            print("3. Reporte")
             print("0. Salir")
             seleccion = int(input("Selecciona una opcion: "))
             if seleccion == 1:
@@ -267,6 +296,13 @@ while True:
                     print("Categoria agregado correctamente.")
                     if input("¿Desea agregar otra categoria? (s/n): ") == "n":
                         break
+            elif seleccion == 3:
+                print("---Consulta de categorias---")
+                tablas.execute("SELECT * FROM `categorias`")
+                resultado = tablas.fetchall()
+                for i in resultado:
+                    print(f"ID del categoria: {i[0]}, nombre: {i[1]}, descripcion: {i[2]}, num_producto: {i[3]}, imagen: {i[4]}")
+                input("Presione enter para continuar.")        
             elif seleccion == 0:
                 break
     elif seleccion == 5:
@@ -275,6 +311,7 @@ while True:
             print("Bienvenido al sistema de empleados.")
             print("1. Consultar")
             print("2. Insertar")
+            print("3. Reporte")
             print("0. Salir")
             seleccion = int(input("Selecciona una opcion: "))
             if seleccion == 1:
@@ -337,6 +374,13 @@ while True:
                     print("Empleado agregado correctamente.")
                     if input("¿Desea agregar otro Empleado? (s/n): ") == "n":
                         break
+            elif seleccion == 3:
+                print("---Consulta de empleados---")
+                tablas.execute("SELECT * FROM `empleados`")
+                resultado = tablas.fetchall()
+                for i in resultado:
+                    print(f"id_empleado: {i[0]}, Nombre: {i[1]}, Apellido: {i[2]}, correo_electronico: {i[3]}, Teléfono: {i[4]}, Cargo: {i[5]}, Salario: {i[6]}, Fecha de contratacion: {i[7]}")
+                input("Presione enter para continuar.")        
             elif seleccion == 0:
                 break
     elif seleccion == 0:
