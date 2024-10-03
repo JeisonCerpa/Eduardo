a1b0e64206b10efa10331bdd777ae2860a0599c8
Jeison Cerpa <cerpajeison8@gmail.com>
2024-10-02 21:36:03 -0500

    Saludo Tkinter

--- FASE 3/Python/Tkinter/Saludo.py
+++ FASE 3/Python/Tkinter/Saludo.py
@@ -1,24 +1,49 @@
 import tkinter as tk
 
 def saludar():
-    nombre = entrada.get()
+    nombre = entry.get()
     mensaje = f'Hola {nombre}!'
-    
     resultado.delete("1.0", tk.END)
     resultado.insert(tk.END, str(mensaje))
     
+def on_entry_click(event):
+    if entry.get() == 'Escribe tu nombre...':
+       entry.delete(0, tk.END)
+       entry.insert(0, '')
+       entry.config(fg = 'red')
+       
+def on_focusout(event):
+    if entry.get() == '':
+        entry.insert(0, 'Escribe tu nombre...')
+        entry.config(fg = 'red')
+    
 ventana = tk.Tk()
-tk.Label(ventana, text="Entrada de datos").grid(row=1, column=0)
-entrada = tk.Entry(ventana)
-entrada.grid(row=1, column=1)
+ventana.geometry("500x250")
+ventana.resizable(False, False)
+ventana.title("Saludo")
 
-tk.Label(ventana, text="Resultado").grid(row=1, column=5, padx=5, pady=10)
-resultado = tk.Text(ventana, height=1, width=15)
-resultado.grid(row=1, column=6, pady=10)
+cuadro = tk.LabelFrame(ventana, text="Entrada de Datos", padx=10, pady=10)
+cuadro.grid(row=0, column=0, padx=20, pady=20)
 
-tk.Label(ventana, text="Enviar").grid(row=2, column=0)
-boton_calcular = tk.Button(ventana, text="Enviar", command=saludar)
-boton_calcular.grid(row=2, column=1)
+entry = tk.Entry(cuadro, bg="lightgreen", fg="red")
+entry.insert(0, 'Escribe tu nombre...')
+entry.grid(row=0, column=0, padx=10, pady=10)
+
+entry.bind('<FocusIn>', on_entry_click)
+entry.bind('<FocusOut>', on_focusout)
+
+cuadro2 = tk.LabelFrame(ventana, text="Resultado", padx=10, pady=10)
+cuadro2.grid(row=0, column=1, padx=20, pady=20)
+
+resultado = tk.Text(cuadro2, height=1, width=20, bg="cyan")
+resultado.grid(row=0, column=0)
+
+cuadro3 = tk.LabelFrame(ventana, text="Enviar", padx=10, pady=10)
+cuadro3.grid(row=1, column=0, padx=20, pady=20)
+
+boton_calcular = tk.Button(cuadro3, text="Enviar", command=saludar, background="cyan", width=20)
+boton_calcular.grid(row=0, column=0)
+
 
 ventana.mainloop()
 
--- FASE 3/Python/Tkinter/prueba.py
+++ FASE 3/Python/Tkinter/prueba.py
@@ -1,23 +1,43 @@
 import tkinter as tk
 
-root = tk.Tk()
-root.geometry("500x300")  # Tamaño de la ventana
+def saludar():
+    nombre = entrada.get()
+    mensaje = f'Hola {nombre}!'
+    
+    resultado.delete("1.0", tk.END)
+    resultado.insert(tk.END, str(mensaje))
+    
+def on_entry_click(event):
+    if entrada.get() == 'Escribe tu nombre...':
+       entrada.delete(0, tk.END)
+       entrada.insert(0, '')
+       entrada.config(fg = 'red')
+       
+def on_focusout(event):
+    if entrada.get() == '':
+        entrada.insert(0, 'Escribe tu nombre...')
+        entrada.config(fg = 'red')
+    
+ventana = tk.Tk()
+ventana.geometry("500x250")
+ventana.resizable(False, False)
+ventana.title("Saludo")
 
-# Configurar las filas y columnas para que crezcan proporcionalmente
-root.grid_rowconfigure(0, weight=1)  # Espacio superior
-root.grid_rowconfigure(3, weight=1)  # Espacio inferior
-root.grid_columnconfigure(0, weight=1)  # Espacio izquierda
-root.grid_columnconfigure(3, weight=1)  # Espacio derecha
+cuadro = tk.LabelFrame(ventana, text="Entrada de Datos", padx=10, pady=10).grid(row=0, column=0, padx=20, pady=20)
 
-# Label centrado
-label = tk.Label(root, text="Hola, mundo!")
-label.grid(row=1, column=1, columnspan=2, pady=10)  # Centrado con columnspan
+entrada = tk.Entry(cuadro, bg="lightgreen", fg="red")
+entrada.insert(0, 'Escribe tu nombre...')
+entrada.grid(row=0, column=0, padx=10, pady=10)
 
-# Label "Primer número:" y Entry en la misma fila
-label_numero = tk.Label(root, text="Primer número:")
-label_numero.grid(row=2, column=1, padx=10, pady=10, sticky="e")  # Alineado a la derecha
+cuadro2 = tk.LabelFrame(ventana, text="Resultado", padx=10, pady=10).grid(row=0, column=1, padx=20, pady=20)
 
-entry_numero = tk.Entry(root)
-entry_numero.grid(row=2, column=2, padx=10, pady=10, sticky="w")  # Alineado a la izquierda
+resultado = tk.Text(cuadro2, height=1, width=20, bg="cyan")
+resultado.grid(row=0, column=0)
 
-root.mainloop()
+cuadro3 = tk.LabelFrame(ventana, text="Enviar", padx=10, pady=10).grid(row=1, column=0, padx=20, pady=20)
+
+boton_calcular = tk.Button(cuadro3, text="Enviar", command=saludar, background="cyan", width=20)
+boton_calcular.grid(row=0, column=0)
+
+ventana.mainloop()
+
